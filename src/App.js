ad0face3bab4db93e30004e270234b7cb7fbd032
BrandontMitchell <btmbaseball99@gmail.com>
2019-08-04 08:49:46 -0700

    add init navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       Brandon Mitchell's Website
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const NavBar = () => {
+    return (
+    <div>
+        Test
+    </div>
+    );
+};
+
+export default NavBar;
