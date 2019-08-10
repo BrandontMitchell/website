a849edc713d2cbe89a48aca228fb4c5c5abc7234
BrandontMitchell <btmbaseball99@gmail.com>
2019-08-10 09:50:32 -0700

    format terminal app

--- src/components/TerminalApp.js
+++ src/components/TerminalApp.js
@@ -4,25 +4,38 @@ import Terminal from 'terminal-in-react';
 const TerminalApp = () => {
     const showMsg = () => 'hi im stuff';
     return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>  
-        <Terminal
-          color='green'
-          backgroundColor='black'
-          barColor='black'
-          style={{ fontWeight: "bold", fontSize: "1em" }}
-          commands={{
-            'open-google': () => window.open('google.com', '_blank'),
-            showmsg: showMsg,
-            popup: () => alert('Terminal in React')
-          }}
-          descriptions={{
-            'open-google': 'opens google.com',
-            showmsg: 'shows a message',
-            alert: 'alert', popup: 'alert'
-          }}
-          msg='Hello World! type help for list of commands'
-        />
-      </div>
+        <div style={
+            {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh"
+            }
+        }>
+            <Terminal color='green' backgroundColor='black' barColor='black'
+                style={
+                    {
+                        fontWeight: "bold",
+                        fontSize: "1em"
+                    }
+                }
+                commands={
+                    {
+                        'open-google': () => window.open('google.com', '_blank'),
+                        showmsg: showMsg,
+                        popup: () => alert('Terminal in React')
+                    }
+                }
+                descriptions={
+                    {
+                        'open-google': 'opens google.com',
+                        showmsg: 'shows a message',
+                        alert: 'alert',
+                        popup: 'alert'
+                    }
+                }
+                msg='Hello World! type help for list of commands'/>
+        </div>
     );
 }
 
