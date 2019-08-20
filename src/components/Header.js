c0c83e78d718c9ab54042abea93cdb2e77bb6560
BrandontMitchell <btmbaseball99@gmail.com>
2019-08-19 21:02:36 -0700

    add header pic/links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-
-const Header = () => {
+const useStyles = makeStyles({
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 250,
+    },
+  });
+  
+  export default function Header() {
+    const classes = useStyles();
+  
     return (
-        <div>Header</div>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://myedmondsnews.com/wp-content/uploads/2017/02/Brandon-Mitchell-Ckairmont-McKenna.jpg"
+            title="Profile Picture"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Brandon Mitchell
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Keyboard shortcut enthusiast
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
     );
-}
-
-export default Header;
+  }
