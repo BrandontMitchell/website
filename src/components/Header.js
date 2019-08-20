fd2ab8806201eb1a22f2ce186708b231ab416cfc
BrandontMitchell <btmbaseball99@gmail.com>
2019-08-19 21:13:10 -0700

    remove card from material-ui, doesnt look good

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,24 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
     card: {
       maxWidth: 345,
+      textAlign: 'center',
+      alignContent: 'inline',
     },
     media: {
-      height: 250,
+      height: 140,
     },
   });
-  
-  export default function Header() {
+
+const Header = () => {
     const classes = useStyles();
-  
     return (
-      <Card className={classes.card}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image="https://myedmondsnews.com/wp-content/uploads/2017/02/Brandon-Mitchell-Ckairmont-McKenna.jpg"
-            title="Profile Picture"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Brandon Mitchell
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Keyboard shortcut enthusiast
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
+        <div className={classes.card}>
+            <h1>Brandon Mitchell</h1>
+        </div>
     );
-  }
+}
+
+export default Header;
