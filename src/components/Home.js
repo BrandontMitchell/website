c752dbe6c4768d468e8eeac33ddc787022b7757b
Brandon Mitchell <btmbaseball99@gmail.com>
2019-09-08 21:57:19 -0700

    update about

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
         <div>
             <br /><br /><br />
         
-            <p>Hello! My name is Brandon Mitchell and I am currently a Junior at Claremont McKenna College in Claremont, California studying Applied Mathematics and Computer Science.</p>
+            <p>Hello! Thank you for taking time to visit my (work in progess) website! Built from scratch in React.js to help practice, as I am still quite new to it. My name is Brandon Mitchell and I am currently a Junior at Claremont McKenna College in Claremont, California studying Applied Mathematics and Computer Science.</p>
 
             <br /><br /><br />
             <br /><br /><br />
