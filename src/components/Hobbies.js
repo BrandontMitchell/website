33c6227628b8f71c49ef3f95648de0d1b0d6a22f
Brandon Mitchell <btmbaseball99@gmail.com>
2019-09-09 17:51:38 -0700

    update hobbies with 3 projects

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -10,8 +10,13 @@ const Hobbies = () => {
                 <li>
                     My favorite project I've made was a crypto currency automated bot: <a href="https://github.com/BrandontMitchell/crypto-b0t">Crypto-R2</a>. The goal of this project was actually to do a mini hackathon with some buddies of mine from Seattle, WA. However after 12 hours of coding, we realized that our setup was completely wrong (it was my friends first time using python), and spent half the time configuring various systems with the right versions of X and Y. After we coded for awhile we realized that this is actually a pretty cool idea. I was in charge of building the UI with the live bitcoin tracking in PyQT5 and Matplotlib. I was also in charge of creating the algorithm in a 'pythonic' way. Luckily the beatuiful library <a href="https://github.com/ccxt/ccxt"> CCXT</a> helped us a great deal. We also had limited testing because I didn't want to spend my friends crypto (the api was connected to his account), however we managed to get some successful transfers!
                 </li>
+                <br/>
                 <li>
-                    Another cool project that I am actually currently working on is
+                    Another cool project that I am actually currently working on is me and my friend's implementation of <a href="https://www.100daysofcode.com/">100 Days of Code.</a> We decided to do things a bit differently, and relive our first encounters with programming in our curriculum. We begin with basic website development fundamentals like HTML, CSS, JS, and PHP. Then move on to Procedural Programming to understand the basic concepts of any programming language like data types, classes and inheritance, loops, and so on. Then we deviate here and go into some cool python libraries such as OpenCV, Pandas and Numpy, Flask, Tweepy, and many others. Then we go back to some more programming skills like data structures and intro to algorithms. After that we continue with the fun in our game development section. We have both put in a lot of effort into this project, and will hopefully be releasing it soon.  
+                </li>
+                <br/>
+                <li>
+                    The last project I want to talk about is a project that my friend and I created at <a href="https://pennapps.com/">PennApps XVIII, </a> a hackathon in 2018. We created <a href="https://github.com/BrandontMitchell/reCharge">ReCharge: </a>a solution to range anxiety that many electric vehicle users face. 
                 </li>
             </ul>
 
